8da63a554e85bedae62d670f24d393f93afc072e
NickP005 <76493745+NickP005@users.noreply.github.com>
2022-05-29 22:31:19 +0200

    now best predator comes out of the simulation. more fun guaranteed

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ class individuo:
         self.creation = time.time()
         self.last_eat = 0
         self.score = 0
+        self.many_childs = 0
+
     def move_x(self, much):
         self.coord.x = (self.coord.x + much) % SCREEN_WIDTH
     def move_y(self, much):
@@ -68,14 +70,14 @@ def disegna_individui():
             coord.a = ray + coord.a
             individuo.raycasted.append(raycast(coord, 15, 220, 4, search_for))
 
-def death_ray(start_coord, shift, distance):
+def death_ray(start_coord, shift, distance, type):
     computed_cos = math.cos(math.radians(start_coord.a))
     computed_sin = math.sin(math.radians(start_coord.a))
     victim_coord = False
     for dist in range(distance):
         x = int(start_coord.x + computed_cos * (dist+shift)) % SCREEN_WIDTH
         y = int(start_coord.y + computed_sin * (dist+shift)) % SCREEN_HEIGHT
-        if(window.get_at((x,y)) == (0, 255, 0, 255)):
+        if(type==0 and window.get_at((x,y)) == (0, 255, 0, 255)):
             victim_coord = (x,y)
         pygame.draw.rect(window,(0,0,255),[x,y,1,1])
     return victim_coord
@@ -132,8 +134,8 @@ class compute_move(threading.Thread):
         individuo = self.individuo
         individuo.speed_x += math.cos(math.radians(individuo.coord.a)) * individuo.accelleration
         individuo.speed_y += math.sin(math.radians(individuo.coord.a)) * individuo.accelleration
-        individuo.speed_x *= 0.25
-        individuo.speed_y *= 0.25
+        individuo.speed_x *= 0.15
+        individuo.speed_y *= 0.15
         individuo.coord.x = (individuo.coord.x - individuo.speed_x ) % SCREEN_WIDTH
         individuo.coord.y = (individuo.coord.y - individuo.speed_y ) % SCREEN_HEIGHT
         threadLock.release()
@@ -152,7 +154,7 @@ def check_deaths():
     to_delete_indexes = []
     for posizione in range(len(lista_individui)):
         individuo = lista_individui[posizione]
-        result = death_ray(individuo.coord, 10, 9)
+        result = death_ray(individuo.coord, 10, 9, individuo.type)
         if(result != False and individuo.type==0):
             (x,y) = result
             #now check most close
@@ -176,15 +178,11 @@ def check_deaths():
                 max_predator_score = individuo.score
                 best_predator = copy.deepcopy(individuo)
                 best_predator.energy = 400
-                print(max_predator_score)
-                for id, node in individuo.brain.nodes.items():
-                    print("#",id, " ", node.value)
-                print("CONNECTIONS")
-                for connection in individuo.brain.connections:
-                    print("#",connection.from_id, " -> #", connection.to_id, " ", connection.weight)
             brain_size = len(individuo.brain.connections)
             if(max_brain_score < brain_size):
                 longest_predator = copy.deepcopy(individuo)
+            if(max_reproduced < individuo.many_childs):
+                best_attractive = copy.deepcopy(individuo)
             break
     cache_list = []
     for i in range(len(lista_individui)):
@@ -195,9 +193,19 @@ def check_deaths():
 def compute_resources():
     for individuo in lista_individui:
         if(individuo.type == 0):
-            individuo.energy -= 1
+            if(predatori > 20):
+                individuo.energy -= 1
+            elif(predatori > 6):
+                individuo.energy -= 0.4
+            else:
+                individuo.energy -= 0.02
         else:
-            individuo.energy += 1
+            if(len(lista_individui)-predatori > 10):
+                individuo.energy += 1
+            elif(len(lista_individui)-predatori > 5):
+                individuo.energy += 3
+            else:
+                individuo.energy += 10
         individuo.energy -= abs(individuo.accelleration) * 0.0008
         if(individuo.last_eat > 0):
             individuo.last_eat -= 1
@@ -209,8 +217,11 @@ def make_childrens():
     lunghezza = len(lista_individui)
     for il in range(lunghezza):
         individuoo = copy.copy(lista_individui[il])
-        if(individuoo.type == 0 and individuoo.energy > 410 and individuoo.last_eat == 0):
-            nuovo_individuo = individuo(0)
+        pred_has_to_eat = 410
+        if(predatori > 400):
+            pred_has_to_eat = 490
+        if(individuoo.type == 0 and individuoo.energy > pred_has_to_eat and individuoo.last_eat == 0):
+            nuovo_individuo = individuo()
             nuovo_individuo.brain = neural_brain.mutation_of(copy.deepcopy(individuoo.brain))
             nuovo_individuo.rays = copy.deepcopy(individuoo.rays)
             nuovo_individuo.coord = copy.deepcopy(individuoo.coord)
@@ -222,6 +233,7 @@ def make_childrens():
             randomize_rays(nuovo_individuo.rays)
             individuoo.energy = 310
             individuoo.last_eat = 600
+            individuoo.many_childs += 1
             continue
         elif(individuoo.type == 1 and individuoo.energy > 380):
             nuovo_individuo = individuo(1)
@@ -232,7 +244,8 @@ def make_childrens():
             nuovo_individuo.move_x(random.randint(-50,+50))
             nuovo_individuo.move_y(random.randint(-50,+50))
             randomize_rays(nuovo_individuo.rays)
-            nuova_lista.append(nuovo_individuo)
+            if(len(lista_individui) < 1200):
+                nuova_lista.append(nuovo_individuo)
             continue
         else:
             nuova_lista.append(individuoo)
@@ -242,18 +255,22 @@ def randomize_rays(rays):
     to_randomise = np.random.randint(0, 20)
     if to_randomise >= len(rays)-1:
         return
-    if(to_randomise == 0):
+    if(to_randomise == 0 and rays[0] != -360):
         rays[0] = np.random.randint(-360, rays[0])
-    elif(to_randomise == 5):
+    elif(to_randomise == 5 and rays[0] != 360):
         rays[5] = np.random.randint(360, rays[5])
-    else:
-        rays[to_randomise] = np.random.randint(rays[to_randomise-1], rays[to_randomise+1])
+    elif rays[to_randomise-1] != rays[to_randomise+1]:
+        cache = np.random.randint(rays[to_randomise-1], rays[to_randomise+1])
+        if(cache != rays[to_randomise-1] and cache != rays[to_randomise+1]):
+            rays[to_randomise] = cache
 
 predatori = 0
 max_predator_score = 0
 best_predator = None
 max_brain_score = 0
 longest_predator = None
+max_reproduced = 0
+best_attractive = None
 
 def many_predators():
     global predatori
@@ -286,26 +303,48 @@ while not game_over:
                 do_fast = not do_fast
             #lista_individui[0].coord.x = event.pos[0]
             #lista_individui[0].coord.y = event.pos[1]
-    """
+        if event.type == pygame.MOUSEBUTTONUP:
+            pos = pygame.mouse.get_pos()
+            (mouse_x,mouse_y) = pos
+            print("x",mouse_x,"y",mouse_y)
+            point1 = np.array((mouse_x,mouse_y))
+            for potenziale in lista_individui:
+                point2 = np.array((potenziale.coord.x, potenziale.coord.y))
+                dist = np.linalg.norm(point1 - point2)
+                if(dist < 10):
+                    individ = copy.deepcopy(potenziale)
+                    print("LIFE DATA - ", individ.type)
+                    print("energy",individ.energy)
+                    print("many childs", individ.many_childs)
+                    print("rays", individ.rays)
+                    print("NODES")
+                    for id, node in individ.brain.nodes.items():
+                        print("#",id, " ", node.layer, " - ", node.value)
+                    print("CONNECTIONS")
+                    for connection in individ.brain.connections:
+                        print("#",connection.from_id, " -> #", connection.to_id, " ", connection.weight)
     if(do_fast):
-        oggetto.tick(280)
+        clock.tick(280)
     else:
-        oggetto.tick(40)
-    """
-    clock.tick(120)
+        clock.tick(0)
+
     window.fill((255, 255, 255))
     disegna_individui()
     check_deaths()
     compute_brains()
-    compute_resources()
-    make_move()
+    if(do_fast):
+        compute_resources()
+        make_move()
     make_childrens()
     many_predators()
-    if(predatori == 0): #disabilitare se si vuole più realistico
+    if(predatori < 3): #disabilitare se si vuole più realistico
+        print("aggiungo predatori")
         lista_individui.append(copy.deepcopy(best_predator))
         lista_individui.append(copy.deepcopy(longest_predator))
-        lista_individui.append(individuo())
-        lista_individui.append(copy.deepcopy(best_predator))
+        lista_individui.append(individuo().move_x(random.randint(-50,+50)))
+        cc = copy.deepcopy(best_predator)
+        cc.energy = 600
+        lista_individui.append(cc)
         lista_individui.append(copy.deepcopy(longest_predator))
         lista_individui.append(individuo())
     text_surface = my_font.render(str(predatori) + ' - ' + str(len(lista_individui)-predatori) + ' - '+ str(len(lista_individui)) + ' | ' + str(clock.get_fps()) + ' FPS', False, (0, 0, 0))
